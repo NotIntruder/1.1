afb283af1048e893a612d2d2e27e7d79f248788e
Intruder. <clasher772yt@gmail.com>
2026-05-22 17:41:02 +0530

    Async Stream Parser
    Created the underlying miniature model of a stream parser.
    
    PS: I'm rusty in kotlin it's been a while I'll get back to it though

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,55 @@
 package main
-import java.io.FileInputStream;
 
-fun main () {
+import java.nio.charset.StandardCharsets
+import java.io.FileInputStream
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 
+
+fun main() {
     val messages = FileInputStream("messages.txt")
+    val lines = getLinesChannel(messages)
+
+    runBlocking {
+        for (line in lines) {
+            println("read: $line")
+        }
+    }
+}
+
+fun getLinesChannel(f: FileInputStream): Channel<String> {
+    var stringHelper = ""
     val buffer = ByteArray(8)
-    var printHelper = ""
+    val channel = Channel<String>()
 
-    while (true) {
-        val read = messages.read(buffer)
+    GlobalScope.launch {
+        while (true) {
+            val read = f.read(buffer)
 
-        if (read == -1) {
-            break
+            if (read == -1) {
+                break
+            }
+            //println("read: ${String(buffer, 0, read)}")
+            /*Get line data till "/n"
+            then print the entire line
+            then we repeat
+            */
+            stringHelper += String(buffer, 0, read, StandardCharsets.UTF_8)
+
+            while (stringHelper.contains('\n')) {
+                val indexOf = stringHelper.indexOf('\n')
+                channel.send(stringHelper.substring(0, indexOf))
+                stringHelper = stringHelper.substring(indexOf + 1)
+            }
+
         }
-        //println("read: ${String(buffer, 0, read)}")
-        /*Get line data till "/n";
-        then print the entire line
-        then we repeat
-        */
-        printHelper += String(buffer, 0, read)
 
-        while (printHelper.contains('\n')) {
-            var indexOf = printHelper.indexOf('\n')
-            println("read: ${printHelper.substring(0, indexOf)}")
-            printHelper = printHelper.substring(indexOf + 1)
+        if (stringHelper.isNotEmpty()) {
+            channel.send(stringHelper)
         }
 
+        channel.close()
+        f.close()
     }
-
-    if (printHelper.isNotEmpty()) {
-        println("read: $printHelper")
-    }
-
-        messages.close()
+    return channel
 }
